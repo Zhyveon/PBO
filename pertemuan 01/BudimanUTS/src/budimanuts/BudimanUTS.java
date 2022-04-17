4a26ffc57c1fd219955a4109aee7abbd9fc59722
Zhyveon <limbudiman80@gmail.com>
2022-04-17 20:15:26 +0700

    test

---
+++ pertemuan 01/BudimanUTS/src/budimanuts/BudimanUTS.java
@@ -0,0 +1,10 @@
+
+package budimanuts;
+public class BudimanUTS {
+    public static void main(String[] args) {
+int a = 10;
+byte b = (byte) (a+1);
+    System.out.println(b);
+    }
+
+}
--- pertemuan 01/test 1/src/test/pkg1/Test1.java
+++ pertemuan 01/test 1/src/test/pkg1/Test1.java
@@ -2,19 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package test.pkg1;
 
 /**
  *
  * @author Zhyveon
  */
-public class Halloworld {
+public class Test1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    System.out.println("hallo dunia satu keluarga");
         // TODO code application logic here
-System.out.println("(/*-*)/");
     }
     
 }
